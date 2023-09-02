125d65e9b51b0f1817d7c609f17dea791baca91c
SvetoCopy <65361271+SvetoCopy@users.noreply.github.com>
2023-09-03 00:47:37 +0300

    uncomment

--- main.cpp
+++ main.cpp
@@ -7,15 +7,5 @@ int main()
 
     TEST_SQUARE test2;
     test2.TEST();
-
-
-//	Coeffs inp{0, 0, 0};
-//	Roots x{0, 0 , 0};
-//	while (scanf("%lg %lg %lg", &inp.a, &inp.b, &inp.c) < 3) {
-//		printf("Incorrect input!\n");
-//		while (getchar() != '\n'){}
-//
-//	}
-//	print_roots(solve_square(inp, &x), x);
 }
 
